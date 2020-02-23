1235f3f9639c7be70ea73095e0a9fccc9ce48828
J Pablo <jpm_caprico@hotmail.com>
2020-02-22 21:53:43 -0600

    template admin

---
+++ admin/admin-main.php
@@ -0,0 +1,66 @@
+<?php include('templates/head.php'); ?>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- BARRA DE NAVEGACION -->
+  <?php include('templates/barra_navegacion.php'); ?>
+  
+  <!-- BARRA LATERAL -->
+  <?php include('templates/aside.php'); ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Página en blanco</h1>
+          </div>
+          <!-- <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Blank Page</li>
+            </ol>
+          </div> -->
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Título</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fas fa-minus"></i></button>
+            <!-- <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fas fa-times"></i></button> -->
+          </div>
+        </div>
+        <div class="card-body">
+          Empieza a crear tu increible aplicación!
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Footer
+        </div>
+        <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- FOOTER -->
+  <?php include('templates/footer.php'); ?>
+
+  <!-- PERSONALIZACION -->
+  <?php include('templates/personalize.php'); ?>
+
+<?php include('templates/foot.php'); ?>
---
+++ admin/templates/foot.php
@@ -0,0 +1,13 @@
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="js/demo.js"></script>
+</body>
+</html>
