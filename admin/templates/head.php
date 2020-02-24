0301b462a6a3f81909e62715b29186d1032ed329
J Pablo <jpm_caprico@hotmail.com>
2020-02-23 21:56:04 -0600

    editando el aside

--- admin/templates/head.php
+++ admin/templates/head.php
@@ -15,5 +15,7 @@
   <link rel="stylesheet" href="css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- estilos personalizados -->
+  <link rel="stylesheet" href="css/admin.css">
 </head>
 <body class="hold-transition sidebar-mini sidebar-collapse">
