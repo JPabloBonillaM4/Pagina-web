b5410110158eeafb14b5f08774c6385db77f0779
J Pablo <jpm_caprico@hotmail.com>
2020-05-14 22:15:18 -0500

    Mejorando form crear administrador

--- admin/crear-admin.php
+++ admin/crear-admin.php
@@ -13,48 +13,60 @@
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1>Crear administrador</h1>
-            <small class="text-muted font-italic">Favor de llenar el formulario completo para registrar un nuevo administrador</small>
+            <small class="font-italic text-danger"> <span>*</span> Favor de llenar el formulario completo para registrar un nuevo administrador</small>
           </div>
         </div>
       </div>
     </section>
 
-    <section class="content col-md-6">
-        <div class="card card-info">
-            <div class="card-header">
-                <h3 class="card-title">Nuevo administrador</h3>
-            </div>
+    <section class="content">
+      <div class="row">
+        <div class="card card-info col-md-6 m-0">
+          <div class="card-header">
+              <h3 class="card-title">Nuevo administrador</h3>
+          </div>
+          <div class="card-body">
             <form class="form-horizontal" name="crear-admin" id="crear-admin">
-                <div class="card-body">
-                    <div class="card-body">
-                        <!-- usuario -->
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-user"></i></span>
-                            </div>
-                            <input type="text" class="form-control" id="user" name="user" data-name="usuario" placeholder="Usuario">
-                        </div>
-                        <!-- contraseña -->
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-key"></i></span>
-                            </div>
-                            <input type="password" class="form-control" id="password" name="password" data-name="contraseña" placeholder="Contraseña">
-                        </div>
-                        <!-- nombre -->
-                        <div class="form-group">
-                            <label for="name">Nombre:</label>
-                            <input type="text" class="form-control" id="name" name="name" data-name="nombre" placeholder="Escriba nombre completo...">
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer d-flex justify-content-center">
-                    <button class="btn btn-outline-success" id="agregar_admin">Agregar</button>
-                </div>
+              <!-- usuario -->
+              <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="fas fa-user"></i></span>
+                  </div>
+                  <input type="text" class="form-control" id="user" name="user" data-name="usuario" placeholder="Usuario">
+              </div>
+              <!-- contraseña -->
+              <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="fas fa-key"></i></span>
+                  </div>
+                  <input type="password" class="form-control" id="password" name="password" data-name="contraseña" placeholder="Contraseña">
+                  <div class="input-group-append">
+                    <button id="show_password" class="btn btn-info" type="button"> <span class="fa fa-eye-slash icon_show_password"></span> </button>
+                  </div>
+              </div>
+              <!-- correo -->
+              <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="fas fa-at"></i></span>
+                  </div>
+                  <input type="mail" class="form-control" id="email" name="email" data-name="correo electrónico" placeholder="Correo electrónico">
+              </div>
+              <!-- nombre -->
+              <div class="form-group">
+                  <label for="name">Nombre:</label>
+                  <input type="text" class="form-control" id="name" name="name" data-name="nombre" placeholder="Escriba nombre completo...">
+              </div>
             </form>
+          </div>
+          <div class="card-footer d-flex justify-content-center">
+            <button class="btn btn-outline-success" id="agregar_admin">Agregar</button>
+          </div>
         </div>
+        <div class="col-md-6">
+          <img class="img-fluid" src="img/admins.gif" alt="imagen-administador">
+        </div>
+      </div>
     </section>
-
   </div>
 
   <!-- FOOTER -->
--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,7 +1,7 @@
 (()=>{
-
     // Acciones
     $('#agregar_admin').click(saveAdmin);
+    $('#show_password').click(showPassword);
 
     // funcion general para obtener datos del formulario
     function getData(form){
@@ -22,7 +22,7 @@
 
                 if(element == 0 || typeof element == 'undefined' || element == null)
                 {
-                    alert(`El campo ${campo} esta vacío, favor de llenarlo`);
+                    alert(`El campo ${campo} está vacío, favor de llenarlo`);
                     $(`#${key}`).focus();
                     return false;
                 }
@@ -32,15 +32,29 @@
         return data;
     };
 
+    function emailValidation(correo){
+        let validation = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(correo.val())
+        console.log(validation);
+        if(validation)
+        {
+            return true;
+        }else{
+            alert('El correo que ingresó debe ser correcto');
+            correo.focus();
+            return false;
+        }
+    }
+
     function saveAdmin(event){
         event.preventDefault();
-
-        datos = getData('crear-admin');
-        console.log(datos);
+        let datos = getData('crear-admin');
         if(typeof datos === 'object')
         {
+            if(!emailValidation($('#crear-admin #email')))
+                return false;
+
             $.post("controllers/insertar-admin.php",datos,function(response){
-                console.log(response);
+                console.log(JSON.parse(response));
             })
             .fail(function(){
                 console.log('Error al procesar registro');
@@ -50,5 +64,24 @@
         }
     }
 
+    function showPassword(){
+        let input_password = $('#password'),
+            icon           = $('.icon_show_password');
+        
+        if(input_password.attr('type') == 'password')
+        {
+            input_password.attr('type','text');
+            icon.removeClass('fa-eye-slash').addClass('fa-eye');
+        }
+        else if(input_password.attr('type') == 'text')
+        {
+            input_password.attr('type','password');
+            icon.removeClass('fa-eye').addClass('fa-eye-slash');
+        }
+        else
+        {
+            console.log('No es posible cambiar este tipo de input');
+        }
+    }
 })();
 
